91c2a9ef82297f25877094eb8e96a9d3636dab57
PadmapriyaInbasekaran <padmapriya.inbasekaran@atmecs.com>
2020-09-03 14:25:50 +0530

    New commits

--- src/main/java/employeerecords/DisplayRecords.java
+++ src/main/java/employeerecords/DisplayRecords.java
@@ -2,13 +2,17 @@ package employeerecords;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class DisplayRecords extends AddRecord{
 	public void dispRecords()
 	{
-		for (Map.Entry<String, List<String>> me : m.entrySet()) { 
+		 Set<Map.Entry<String, List<String>>> st = m.entrySet();
+		for (Map.Entry<String, List<String>> me : st) { 
 			System.out.print(me.getKey() + ":"); 
 			System.out.println(me.getValue());
 		}
 	}
 }
+
+
